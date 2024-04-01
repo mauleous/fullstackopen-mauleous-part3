70cbb97ccfbcf01f131448e7f0e5c2e256fcdb1b
mauleous <mauleous@yahoo.com>
2024-04-01 15:43:40 +0300

    Added CORS and make PORT configurable

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 // Morgan configuration
 const morganConfig = (tokens, request, response) => {
@@ -110,7 +112,7 @@ app.post('/api/persons', (request, response) => {
   response.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
